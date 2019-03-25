db96f81c43184683ef9906e46c036b4278256c68
Jackson Bush <jackson@hertzler.com>
2019-03-25 13:34:55 -0400

    attempting to project the files separately

--- createJsonFiles.py
+++ createJsonFiles.py
@@ -20,12 +20,23 @@ def main():
         'shp2json cb_2017_us_state_500k.shp -o states.json', shell=True)
 
     # take the coordinates and turn them into GeoJson Points
-    print(createGeoJsonStringForPoints())
+    # write this out to a file
+    with open('points.json', 'w', encoding='utf-8') as f:
+        f.write(createGeoJsonStringForPoints())
+
+    # now project all three files
+    subprocess.call("geoproject 'd3.geoAlbersUsa().fitSize([960, 600], d)' < nation.json > nation-albers.json", shell=True)
+    subprocess.call("geoproject 'd3.geoAlbersUsa().fitSize([960, 600], d)' < states.json > states-albers.json", shell=True)
+    subprocess.call("geoproject 'd3.geoAlbersUsa().fitSize([960, 600], d)' < points.json > points-albers.json", shell=True)
+
+    # combine these three geojson files into one single topojson file
+    subprocess.call('geo2topo nation=nation-albers.json states=states-albers.json points=points-albers.json > combined.json', shell=True)
+
     # combine the nation GsoJson and states GeoJson and locations GeoJson into one FeatureCollection
     # just get features attribute from both nation.json and states.json and add them to points geojson
     # or perhaps should just create one topojson file first and then project that all at once?
-    nationJson = json.load(open('nation.json', 'r', encoding='utf-8'))
-    statesJson = json.load(open('nation.json', 'r', encoding='utf-8'))
+    # nationJson = json.load(open('nation.json', 'r', encoding='utf-8'))
+    # statesJson = json.load(open('states.json', 'r', encoding='utf-8'))
     # project single GeoJson file
     # then break features back out into separate geojson files
     # turn these GeoJson files into topojson files and combine them into a single topojson file
